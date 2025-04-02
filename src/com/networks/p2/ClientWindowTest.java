4f2369839f235488d520867067d5b7defd1501b8
jaysalgado <jaysalgado97@gmail.com>
2025-04-02 13:54:11 -0400

    first

---
+++ src/com/networks/p2/ClientWindowTest.java
@@ -0,0 +1,9 @@
+package com.networks.p2;
+
+public class ClientWindowTest
+{
+    public static void main(String[] args)
+    {
+        ClientWindow window = new ClientWindow();
+    }
+}
---
+++ src/com/networks/p2/Main.java
@@ -0,0 +1,7 @@
+package com.networks.p2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
