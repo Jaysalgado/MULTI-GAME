ef7c7e1822976faaa606bc308d32c0248d7a9e9c
Aban <khanaban505@gmail.com>
2025-04-04 11:57:34 -0400

    Main logic added

--- src/com/networks/p2/Main.java
+++ src/com/networks/p2/Main.java
@@ -3,9 +3,26 @@ package com.networks.p2;
 
 
 public class Main {
-
     public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Usage: java Main [server|client]");
+            return;
+        }
 
+        switch (args[0].toLowerCase()) {
+            case "server":
+                System.out.println("[Main] Starting server.");
+                new Server().startServer();
+                break;
+
+            case "client":
+                System.out.println("[Main] Starting client.");
+                ClientWindow.start();
+                break;
+
+            default:
+                System.out.println("Invalid argument. Use 'server' or 'client'");
+        }
     }
 }
 
